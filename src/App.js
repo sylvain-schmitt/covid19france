c4d59b76885407355b5ec1c05a3331cffd1abe5c
Sylvain Schmitt <devil.9068@gmail.com>
2021-05-03 16:07:35 +0200

    change link for homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Switch>
+              <Route path="/covid19france" exact component={Home} />
+              <Route path="/a-propos" exact component={About} />
+              <Route component={NotFound} />
+          </Switch>
+      </BrowserRouter>
+
   );
-}
+};
 
 export default App;
+
